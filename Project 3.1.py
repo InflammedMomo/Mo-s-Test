c1bc93770940dc5de671c42a59d1b8d47b985c80
InflammedMomo <mohanad.mohamed@torontomu.ca>
2024-12-14 23:04:05 -0500

    12/14/2024

--- Project 3.1.py
+++ Project 3.1.py
@@ -8,36 +8,28 @@ image = cv2.imread('C:\\Users\\super\\Documents\\GitHub\\Mo-s-Test\\Project 3 Da
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Apply thresholding to the image
 _, thresholded = cv2.threshold(gray_image, 110, 255, cv2.THRESH_BINARY)
 
-# Invert mask
 inverted_mask=cv2.bitwise_not(thresholded)
 
-# Perform edge detection using contour detection
 contours, _ = cv2.findContours(inverted_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Find the largest contour (assuming it's the motherboard)
 largest_contour = max(contours, key=cv2.contourArea)
 image_with_contours = image.copy()
 
-# Create an empty mask to extract the board
 mask = np.zeros_like(gray_image)
 cv2.drawContours(mask, [largest_contour], -1, (255, 255, 255), thickness=cv2.FILLED)
 cv2.drawContours(image_with_contours, [largest_contour], -1, (0, 255, 0), 2)
 
-# Extract the motherboard from the background
 board_extracted = cv2.bitwise_and(image, image, mask=mask)
 
-#Image resizing
 target_width, target_height = int(2172/2.5),int(2896/2.5)
 resized_image = cv2.resize(image, (target_width, target_height))
 
-# Display the results
 cv2.imwrite('resized_image.JPEG',resized_image)
 cv2.imwrite('inverted_mask.JPEG',inverted_mask)
 cv2.imwrite('board_extracted.JPEG',board_extracted)
-
+cv2.imwrite('image_with_contours.JPEG',image_with_contours)
 
 # Step 2
 '''
